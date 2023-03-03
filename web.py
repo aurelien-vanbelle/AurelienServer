677de58551967b5a49a2cc892b8d9e22cbbdbf47
Aurel59 <aurelien.vanbelle@epsi.fr>
2023-03-03 13:16:20 +0100

    commit 7

--- web.py
+++ web.py
@@ -1,13 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Hello, World!'
 
-@app.route("/hello", methods=["GET"])
-def say_hello():
-    return jsonify({"msg": "Hello from Flask"})
-
-
-if __name__ == "__main__":
-    # Please do not set debug=True in production
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, host="0.0.0.0")
